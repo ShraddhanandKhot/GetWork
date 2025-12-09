522eaddb286196096b758a6b14c39b19d0a69bcc
ShraddhanandKhot <shraddhanand2836@gmail.com>
2025-12-10 00:07:27 +0530

    Updating UI_5_1

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
 
       {/* Logo */}
       <Link href="/">
-        <div className="flex items-center gap-3 cursor-pointer">
+        <div className="flex items-center gap-0 cursor-pointer">
           <Image
             src="/icons/icon-512x512.png"
             alt="GetWork Logo"
