8692938df30a994bc477f36bb724d6e96176547e
ShraddhanandKhot <shraddhanand2836@gmail.com>
2026-01-05 01:46:22 +0530

    notification update _7

--- app/jobs/[id]/page.tsx
+++ app/jobs/[id]/page.tsx
@@ -1,5 +1,7 @@
 import JobDetailsClient from "./JobDetailsClient";
 
 export default function Page({ params }: any) {
+  console.log("ROUTE PARAMS:", params);
   return <JobDetailsClient jobId={params.id} />;
 }
+
